c0716a1e51b6016ac36c3e7d9c5f646ebb2fd950
bossTodev <bossongsaza@gmail.com>
2024-03-04 01:49:27 +0700

    image fusion 4 success V1

--- imfusion.py
+++ imfusion.py
@@ -1,8 +1,8 @@
 import cv2
 
 # เปิดวิดีโอ
-#cap = cv2.VideoCapture('openCV/fall_vedio.mp4')
-cap = cv2.VideoCapture('openCV/fall_vedio.mp4')
+cap = cv2.VideoCapture('openCV/testfall.mp4')
+#cap = cv2.VideoCapture(0)
 
 # ตรวจสอบว่าสามารถเปิดวิดีโอได้หรือไม่
 if not cap.isOpened():
@@ -11,47 +11,46 @@ if not cap.isOpened():
 
 # กำหนดตัวแปรเพื่อนับเฟรม
 frame_count = 0
-
-# เฟรมปัจจุบัน
-current_frame = None
+init_frame = None
 fusion1 = None
 fusion2 = None
 
+
 while True:
+
+
     # อ่านเฟรมปัจจุบัน
     ret, frame = cap.read()
 
     # ตรวจสอบว่าอ่านเฟรมได้สำเร็จหรือไม่
     if not ret:
         break
+    #reset framecount
+    if frame_count == 10:
+        frame_count = 0
 
     # รวมเฟรมทุกๆ 4 เฟรม
     if frame_count % 4 == 0: #initial frame 0
-        current_frame = frame
-        print('init '+str(frame_count))
+        init_frame = frame
 
-    elif frame_count <= 10:
+    
+    if frame_count % 4 == 1:
+        fusion1 = cv2.addWeighted(init_frame, 0.5, frame, 0.5, 0)
 
-        if frame_count % 4 < 2: #mod 1 & 2
-            fusion1 = cv2.add(current_frame,frame)
-            current_frame = fusion1
-            print(f"fusion1 {frame_count}")
-            print('end1')
+    if frame_count % 4 == 2:
+        fusion2 = cv2.addWeighted(fusion1, 0.5, frame, 0.5, 0)
 
-        elif frame_count % 4 >= 2: #mod 2 & 3
-            fusion2 = cv2.add(fusion1,frame)
-            print(f"fusion2 {frame_count}")
-
-            # แสดงเฟรมที่รวมกัน
-            cv2.imwrite(str(frame_count)+'.png', fusion2)
-            cv2.imshow('fusion',fusion2)
-            cv2.waitKey(1)
-            print('end2')
+    if frame_count % 4 == 3:
+        fusion_full = cv2.addWeighted(fusion2, 0.5, fusion1, 0.5, 0)
+        #cv2.imwrite(str(frame_count)+'.png', fusion_full)
+        cv2.imshow('fusion image',fusion_full)
+        print('count '+str(frame_count))
+    
     # เพิ่มค่า frame_count ขึ้นทีละ 1
     frame_count += 1
-    #reset
-
-
+    # หน่วงเวลา
+    cv2.waitKey(17)
+        
 
 # ปิดวิดีโอ
 cap.release()
